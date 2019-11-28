f14c9065a430a4a60d0e58357c56c733c1819cc0
Raphael Candello <raphael.candello@frotcom.com>
2019-11-28 22:00:55 +0000

    First Dhecatron body

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
   <v-app>
     <v-content>
       <Lightnings/>
+      <Dhecatron/>
     </v-content>
   </v-app>
 </template>
@@ -9,12 +10,14 @@
 <script lang="ts">
 import Vue from 'vue';
 import Lightnings from './components/Lightnings.vue';
+import Dhecatron from './components/Dhecatron.vue';
 
 export default Vue.extend({
   name: 'App',
 
   components: {
     Lightnings,
+    Dhecatron,
   },
 
   data: () => ({
